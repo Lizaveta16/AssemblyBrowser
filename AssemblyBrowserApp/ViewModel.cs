080f1c97e359f85a247cdda66496f0cd234328f9
Lizaveta <lizazaytseva16@gmail.com>
2021-10-21 22:12:37 +0300

    Updated WPF app

--- AssemblyBrowserApp/ViewModel.cs
+++ AssemblyBrowserApp/ViewModel.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
+using AssemblyBrowserLib.Data;
 using Microsoft.Win32;
 
 namespace AssemblyBrowserApp
@@ -14,9 +12,56 @@ namespace AssemblyBrowserApp
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public void OnPropertyChanged(string propertyName = "")
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private readonly AssemblyBrowserLib.AssemblyBrowser _assemblyBrowser = new AssemblyBrowserLib.AssemblyBrowser();
+
+
+        private List<DataContainer> _namespace;
+        public List<DataContainer> Namespaces { get; set; }
+
+        private string _openFile;
+        public string OpenFile
+        {
+            get => _openFile;
+            set
+            {
+                _openFile = value;
+                Namespaces = null;
+                try
+                {
+                    Namespaces = _assemblyBrowser.GetAssemblyInfo(_openFile);
+                }
+                catch (Exception e)
+                {
+                    _openFile = e.Message;
+                    Console.WriteLine(_openFile);
+                }
+                OnPropertyChanged(nameof(Namespaces));
+
+            }
+        }
+        public ICommand OpenFileCommand => new OpenFileCommand(OpenAssembly);
+
+        private void OpenAssembly()
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                Filter = "Assemblies|*.dll;*.exe",
+                Title = "Select assembly",
+                Multiselect = false
+            };
+
+            var isOpen = fileDialog.ShowDialog();
+
+            if (isOpen != null && isOpen.Value)
+            {
+                OpenFile = fileDialog.FileName;
+                OnPropertyChanged(nameof(OpenFile));
+            }
         }
     }
 }
--- AssemblyBrowserLib/Data/MembersInfo/MethodInformation.cs
+++ AssemblyBrowserLib/Data/MembersInfo/MethodInformation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AssemblyBrowserLib.Data.MembersInfo
 {
